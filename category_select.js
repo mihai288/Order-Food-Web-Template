0ebb3eab4d4a1814473cad3caeae6eef127bda52
Mihai <mihai.diaconu05@e-uvt.ro>
2025-01-04 22:23:16 +0200

    Add files via upload

---
+++ category_select.js
@@ -0,0 +1,138 @@
+
+    const parentDiv = document.getElementById('margin-page');
+
+    const aToggle = document.getElementById('category1');
+    aToggle.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+
+        document.getElementById('chicken').style.display = 'block';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'none';
+
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle2 = document.getElementById('category2');
+    aToggle2.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'block';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'none';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle3 = document.getElementById('category3');
+    aToggle3.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'block';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'none';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle4 = document.getElementById('category4');
+    aToggle4.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'block';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'none';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle5 = document.getElementById('category5');
+    aToggle5.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'block';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'none';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle6 = document.getElementById('category6');
+    aToggle6.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'block';
+        document.getElementById('sauces').style.display = 'none';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
+    const aToggle7 = document.getElementById('category7');
+    aToggle7.addEventListener('click', function(){
+        const links = parentDiv.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.background = 'white';
+            link.style.color = 'black';
+            link.style.transform = 'scale(1)';
+        });
+        document.getElementById('chicken').style.display = 'none';
+        document.getElementById('beef').style.display = 'none';
+        document.getElementById('menus').style.display = 'none';
+        document.getElementById('christmas').style.display = 'none';
+        document.getElementById('burger').style.display = 'none';
+        document.getElementById('fries').style.display = 'none';
+        document.getElementById('sauces').style.display = 'block';
+        this.style.background = 'black';
+        this.style.color = 'white';
+        this.style.transform = 'scale(1.1)';
+    });
